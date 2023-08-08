c1aa7d285571541d6a4b25b80397ed14254746c3
janjao937 <janjao937@gmail.com>
2023-08-08 23:44:13 +0700

    forLoop_07-primeNumber

--- 03-forLoop/07-primeNumber/main.js
+++ 03-forLoop/07-primeNumber/main.js
@@ -1,3 +1,58 @@
+
+//ให้เขียนโค้ดเพื่อหาจำนวนเฉพาะตั้งแต่ 1 ถึง 100
+let range = 100;
+let isPrime = true;
+
+for(let i = 1;i<=range;i++)
+{
+    if(i===1) continue;
+
+    isPrime = true;
+    for(let j = 2;j<= Math.sqrt(i);j++)//Check Root
+    {
+        if(i%j === 0)
+        {
+            //Not prime
+            isPrime = false;
+            break;
+        }
+    }
+
+    if(isPrime)
+    {
+        console.log(i);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+//primeNumber.map((x)=>console.log(x));
+
+
+// for(let j = 2;j<=currenNumberChecking;j++)//check num isPrime Or not
+// {
+//    if(range % currenNumberChecking!==0)
+//    {
+//        //not prime
+//        primeNumber.push(currenNumberChecking);
+//        return;
+//    }
+
+// }
+
+
+
+
+
+
 // let arrayNumberForDivide = [2,3,5,7,11,13];
 
 // let primeNumber =[];
